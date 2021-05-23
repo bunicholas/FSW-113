261e2e88ed0a0fe1e1e20b85da69881af30f0fc3
bunicholas <nicholascarroll123@gmail.com>
2021-05-23 15:40:22 -0400

    message

---
+++ variables.js
@@ -0,0 +1,54 @@
+var person1 = {
+    firstName: "Brianna",
+    balance: -54.99,
+    rate: 2.7
+}
+var person2 = {
+    firstName: "Dillon",
+    balance: 1014.94,
+    rate: 5.1,
+}
+var person3 = {
+    firstName: "Dmitri",
+    balance: 0,
+    rate: 7.2,
+}
+var person4 = {
+    firstName: "Hazel",
+    balance: 0,
+    rate: 7.2,
+}
+var person5 = {
+    firstName: "Isaiah",
+    balance: 224.99,
+    rate: 5.1,
+}
+var person6 = {
+    firstName: "Laura",
+    balance: -213.40,
+    rate: 2.5,
+}
+var person7 = {
+    firstName: "Matias",
+    balance: -12.06,
+    rate: 3.1,
+}
+var person8 = {
+    firstName: "Monique",
+    balance: 2220.54,
+    rate: 7.2,
+}
+var person9 = {
+    firstName: "Petra",
+    balance: 500.35, 
+    rate: 4.9,
+}
+var person10 = {
+    firstName: "Reggie",
+    balance: 0, 
+    rate: 7.2,
+}
+function people(person){
+console.log(person.rate)
+}
+people(person10)
